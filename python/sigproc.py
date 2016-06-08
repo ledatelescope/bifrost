9c70bd901f813d535cd5c7be3c421f4d877c58d5
Ben <benbarsdell@gmail.com>
2016-06-08 12:33:40 -0700

    Improvements to sigproc.py, now allows slicing and unpacking.

--- python/sigproc.py
+++ python/sigproc.py
@@ -56,46 +56,64 @@ import numpy as np
 from collections import defaultdict
 
 _string_values = ['source_name',
-                 'rawdatafile']
+                  'rawdatafile']
 _double_values = ['az_start',
-                 'za_start',
-                 'src_raj',
-                 'src_dej',
-                 'tstart',
-                 'tsamp',
-                 'period',
-                 'fch1',
-                 'foff',
-                 'refdm']
+                  'za_start',
+                  'src_raj',
+                  'src_dej',
+                  'tstart',
+                  'tsamp',
+                  'period',
+                  'fch1',
+                  'foff',
+                  'refdm']
 _integer_values = ['nchans',
-                  'telescope_id',
-                  'machine_id',
-                  'data_type',
-                  'ibeam',
-                  'nbeams',
-                  'nbits',
-                  'barycentric',
-                  'pulsarcentric',
-                  'nbins',
-                  'nsamples',
-                  'nifs',
-                  'npuls']
+                   'telescope_id',
+                   'machine_id',
+                   'data_type',
+                   'ibeam',
+                   'nbeams',
+                   'nbits',
+                   'barycentric',
+                   'pulsarcentric',
+                   'nbins',
+                   'nsamples',
+                   'nifs',
+                   'npuls']
 _character_values = ['signed']
 
-_telescopes = defaultdict(lambda : "unknown",
-                          {0:  'fake',
+_data_types = defaultdict(lambda : 'unknown',
+                          {0: 'raw data',
+                           1: 'filterbank',
+                           2: 'time series',
+                           3: 'pulse profile',
+                           4: 'amplitude spectrum',
+                           5: 'complex spectrum',
+                           6: 'dedispersed subbands'})
+_telescopes = defaultdict(lambda : 'unknown',
+                          {0:  'Fake',
                            1:  'Arecibo',
                            2:  'Ooty',
+                           3:  'Nancay',
                            4:  'Parkes',
-                           5:  'Lovell',
+                           5:  'Jodrell',#'Lovell',
+                           6:  'GBT',
                            7:  'GMRT',
-                           52: 'LWA-OV'})
-_machines   = defaultdict(lambda : "unknown",
+                           8:  'Effelsberg',
+                           52: 'LWA-OV',
+                           53: 'LWA-SV'})
+_machines   = defaultdict(lambda : 'unknown',
                           {0:  'FAKE',
                            1:  'PSPM',
                            2:  'WAPP',
-                           3:  'OOTY',
-                           52: 'LWA-OV'})
+                           3:  'AOFTM',
+                           4:  'BPP',
+                           5:  'OOTY',
+                           6:  'SCAMP',
+                           7:  'GMRTFB',
+                           8:  'PULSAR2000',
+                           52: 'LWA-DP',
+                           53: 'LWA-ADP'})
 
 def _header_write_string(f, key):
 	f.write(struct.pack('=i', len(key)))
@@ -172,7 +190,39 @@ def _read_header(f):
 	#	f.seek(header['header_size'], 0) # Seek back to end of header
 	return header
 
+# TODO: Move this elsewhere?
+def unpack(data, nbit):
+	if nbit > 8:
+		raise ValueError("unpack: nbit must be <= 8")
+	if 8 % nbit != 0:
+		raise ValueError("unpack: nbit must divide into 8")
+	if data.dtype not in (np.uint8, np.int8):
+		raise TypeError("unpack: dtype must be 8-bit")
+	if nbit == 8:
+		return data
+	elif nbit == 4:
+		# Note: This technique assumes LSB-first ordering
+		x = data.astype(np.int16)#np.empty(upshape, dtype=np.int16)
+		x = (x | (x <<  8)) & 0x0F0F
+		x = x << 4 # Shift into high bits to avoid needing to sign extend
+		updata = x
+	elif nbit == 2:
+		x = data.astype(np.int32)#np.empty(upshape, dtype=np.int16)
+		x = (x | (x << 16)) & 0x000F000F
+		x = (x | (x <<  8)) & 0x03030303
+		x = x << 6 # Shift into high bits to avoid needing to sign extend
+		updata = x
+	elif nbit == 1:
+		x = data.astype(np.int64)#np.empty(upshape, dtype=np.int16)
+		x = (x | (x << 32)) & 0x0000000F0000000F
+		x = (x | (x << 16)) & 0x0003000300030003
+		x = (x | (x <<  8)) & 0x0101010101010101
+		x = x << 7 # Shift into high bits to avoid needing to sign extend
+		updata = x
+	return updata.view(data.dtype)
+	
 # TODO: Add support for writing
+#       Add support for data_type != filterbank
 class SigprocFile(object):
 	def __init__(self, filename=None):
 		if filename is not None:
@@ -182,10 +232,12 @@ class SigprocFile(object):
 		#         is divided by pack_factor, with dtype set to uint8.
 		self.f = open(filename, 'rb')
 		self.header = _read_header(self.f)
+		self.header_size = self.header['header_size']
 		self.frame_shape = (self.header['nifs'], self.header['nchans'])
 		self.nbit = self.header['nbits']
+		signed = 'signed' in self.header and self.header['signed'] == True
 		if self.nbit >= 8:
-			if 'signed' in self.header and self.header['signed'] == True:
+			if signed:
 				self.dtype  = { 8: np.int8,
 				               16: np.int16,
 				               32: np.float32,
@@ -196,7 +248,7 @@ class SigprocFile(object):
 				               32: np.float32,
 				               64: np.float64}[self.nbit]
 		else:
-			self.dtype = np.uint8
+			self.dtype = np.int8 if signed else np.uint8
 			pack_factor = 8 / self.nbit
 			self.frame_shape = (self.frame_shape[0],
 			                    self.frame_shape[1]/pack_factor)
@@ -210,16 +262,63 @@ class SigprocFile(object):
 		return self
 	def __exit__(self, type, value, tb):
 		self.close()
-	def read(self, nframe):
+	def seek(self, offset, whence=0):
+		if whence == 0:
+			offset += self.header_size
+		self.f.seek(offset, whence)
+	def bandwidth(self):
+		return self.header['nchans'] * self.header['foff']
+	def cfreq(self):
+		return self.header['fch1'] + 0.5*(self.header['nchans']-1)*self.header['foff']
+	def duration(self):
+		return self.header['tsamp'] * self.nframe()
+	def nframe(self):
+		if 'nsamples' not in self.header or self.header['nsamples'] == 0:
+			curpos = self.f.tell()
+			self.f.seek(0, 2) # Seek to end of file
+			frame_bits = self.header['nifs'] * self.header['nchans'] * self.header['nbits']
+			nframe = (self.f.tell() - self.header['header_size'])*8 / frame_bits
+			self.header['nsamples'] = nframe
+			self.f.seek(curpos, 0) # Seek back to where we were
+		return self.header['nsamples']
+	def read(self, nframe_or_start, end=None):
+		if end is not None:
+			start = nframe_or_start or 0
+			self.seek(start * self.frame_nbyte)
+			if end == -1:
+				end = self.nframe()
+			nframe = end - start
+		else:
+			nframe = nframe_or_start
 		data = np.fromfile(self.f, count=nframe*self.frame_size, dtype=self.dtype)
+		nframe = data.size // self.frame_size
 		data = data.reshape((nframe,)+self.frame_shape)
+		nbit = self.header['nbits']
+		if nbit < 8:
+			data = unpack(data, nbit)
 		return data
 	def readinto(self, buf):
 		return self.f.readinto(buf)
 	def __str__(self):
-		hmod = h.copy()
+		hmod = self.header.copy()
+		d = hmod['data_type']
+		hmod['data_type'] = "%i (%s)" % (d, _data_types[d])
 		t = hmod['telescope_id']
 		hmod['telescope_id'] = "%i (%s)" % (t, _telescopes[t])
 		m = hmod['machine_id']
 		hmod['machine_id']   = "%i (%s)" % (m, _machines[m])
 		return '\n'.join(['% 16s: %s' % (key,val) for (key,val) in hmod.items()])
+	def __getitem__(self, key):
+		if isinstance(key, type("")): # Header key lookup
+			return self.header[key]
+		elif isinstance(key, int): # Extract one time slice
+			return self.read(key, key+1)[0]
+		elif isinstance(key, slice): # 1D slice
+			start = key.start if key.start is not None else  0
+			stop  = key.stop  if key.stop  is not None else -1
+			data = self.read(start, stop)
+			#data = self.read(stop) if start == 0 else \
+			#       self.read(start, key.stop)
+			return data[::key.step]
+		elif isinstance(key, tuple): # ND key
+			raise NotImplementedError
