bfe2cab0156053b58c1f6eca22b786121c77184b
mdtmc <miles.cranmer@gmail.com>
2016-06-24 07:56:09 -0700

    Descriptive names and doxygen comments

--- src/test_fft.cu
+++ src/test_fft.cu
@@ -6,143 +6,197 @@
 #include <gtest/gtest.h>
 #include <bifrost/common.h>
 
+/*! \brief Simple test of 2 dimensional real data
+ *  through a forward bfFFT.
+ */
 TEST(FFTTest, Handles2dReal)
 {
-    BFarray my_data;
-    BFarray out_data;
-    BFreal set_data[3][2] = 
+    /// Set up the test with a 3x2 element real array
+    BFarray dataAndDescription;
+    BFarray outputDataAndDescription;
+    cufftReal hostData[3][2] = 
         {{1,2},{2,3},{3,4}};
-    BFreal** some_data;
-    BFcomplex* odata;
-    cudaMalloc((void**)&some_data, sizeof(BFreal)*6);
-    cudaMalloc((void**)&odata, sizeof(BFcomplex)*6);
+    cufftReal** deviceData;
+    cufftComplex outputHostData[3][2] = {};
+    cufftComplex** outputDeviceData;
+    /// Load the test array to the GPU
+    cudaMalloc((void**)&deviceData, sizeof(cufftReal)*6);
+    cudaMalloc((void**)&outputDeviceData, sizeof(cufftComplex)*6);
     cudaMemcpy(
-        some_data, set_data, 
-        sizeof(BFreal)*6, cudaMemcpyHostToDevice);
-    my_data.data = some_data;
-    my_data.space = BF_SPACE_CUDA;
-    my_data.shape[0] = 3;
-    my_data.shape[1] = 2;
-    my_data.dtype = 0;
-    my_data.ndim = 2;
-    my_data.strides[0] = 2*sizeof(BFreal);
-    my_data.strides[1] = sizeof(BFreal);
-    out_data = my_data;
-    out_data.data = odata;
-    out_data.dtype = 1;
-    out_data.strides[0] = 2*sizeof(BFcomplex);
-    out_data.strides[1] = sizeof(BFcomplex);
-    bfFFT(&my_data, &out_data, FFT_FORWARD);
-    cufftComplex localdata[3][2] = {};
+        deviceData, hostData, 
+        sizeof(cufftReal)*6, cudaMemcpyHostToDevice);
+    /// Describe our input and output data
+    dataAndDescription.data = deviceData;
+    dataAndDescription.space = BF_SPACE_CUDA;
+    dataAndDescription.shape[0] = 3;
+    dataAndDescription.shape[1] = 2;
+    dataAndDescription.dtype = 0;
+    dataAndDescription.ndim = 2;
+    dataAndDescription.strides[0] = 2*sizeof(cufftReal);
+    dataAndDescription.strides[1] = sizeof(cufftReal);
+    outputDataAndDescription = dataAndDescription;
+    outputDataAndDescription.data = outputDeviceData;
+    outputDataAndDescription.dtype = 1;
+    outputDataAndDescription.strides[0] = 2*sizeof(cufftComplex);
+    outputDataAndDescription.strides[1] = sizeof(cufftComplex);
+
+    /// Perform the forward FFT in the separate output object
+    bfFFT(&dataAndDescription, &outputDataAndDescription, FFT_FORWARD);
+    /// Download the data back from the GPU
     cudaMemcpy(
-        localdata, (cufftComplex*)out_data.data, 
+        outputHostData, (cufftComplex*)outputDataAndDescription.data, 
         sizeof(cufftComplex)*6, cudaMemcpyDeviceToHost);
-    EXPECT_EQ(cuCrealf(localdata[0][0]),15);
+
+    /// Did the FFT produce the correct result?
+    /// We check this only with one value.
+    EXPECT_EQ(cuCrealf(outputHostData[0][0]),15);
 }
 
+/*! \brief Simple test of 1 dimensional real data
+ *  through a forward bfFFT.
+ */
 TEST(FFTTest, Handles1dReal)
 {
-    BFarray my_data;
-    BFarray out_data;
-    BFreal set_data[4] = {1,3,6,4.5};
-    BFreal* some_data;
-    BFcomplex* odata;
-    cudaMalloc((void**)&some_data, sizeof(BFreal)*5);
-    cudaMalloc((void**)&odata, sizeof(BFcomplex)*3);
+    /// Set up the test with a 4 element real array
+    BFarray dataAndDescription;
+    BFarray outputDataAndDescription;
+    cufftReal hostData[4] = {1,3,6,4.5};
+    cufftReal* deviceData;
+    cufftComplex outputHostData[3];
+    cufftComplex* outputDeviceData;
+    /// Load this array to the GPU
+    cudaMalloc((void**)&deviceData, sizeof(cufftReal)*5);
+    cudaMalloc((void**)&outputDeviceData, sizeof(cufftComplex)*3);
     cudaMemcpy(
-        some_data, set_data, 
-        sizeof(BFreal)*4, cudaMemcpyHostToDevice);
-    my_data.data = some_data;
-    my_data.space = BF_SPACE_CUDA;
-    my_data.shape[0] = 4;
-    my_data.dtype = 0;
-    my_data.ndim = 1;
-    my_data.strides[0] = sizeof(BFreal);
-    out_data = my_data;
-    out_data.data = odata;
-    out_data.dtype = 1;
-    out_data.strides[0] = sizeof(BFcomplex);
-    bfFFT(&my_data, &out_data, FFT_FORWARD);
-    cufftComplex localdata[3] = {};
+        deviceData, hostData, 
+        sizeof(cufftReal)*4, cudaMemcpyHostToDevice);
+    /// Describe our input and output data
+    dataAndDescription.data = deviceData;
+    dataAndDescription.space = BF_SPACE_CUDA;
+    dataAndDescription.shape[0] = 4;
+    dataAndDescription.dtype = 0;
+    dataAndDescription.ndim = 1;
+    dataAndDescription.strides[0] = sizeof(cufftReal);
+    /// Output data is a different type, so requires
+    /// a separate output object
+    outputDataAndDescription = dataAndDescription;
+    outputDataAndDescription.data = outputDeviceData;
+    outputDataAndDescription.dtype = 1;
+    outputDataAndDescription.strides[0] = sizeof(cufftComplex);
+
+    /// Perform the forward FFT into the separate output data
+    bfFFT(&dataAndDescription, &outputDataAndDescription, FFT_FORWARD);
+    /// Download the data back from the GPU
     cudaMemcpy(
-        localdata, (cufftComplex*)out_data.data, 
+        outputHostData, (cufftComplex*)outputDataAndDescription.data, 
         sizeof(cufftComplex)*3, cudaMemcpyDeviceToHost);
-    EXPECT_EQ((int)cuCimagf(localdata[1]), 1);
+
+    /// Did the FFT produce the correct result?
+    /// We check this only with one value.
+    EXPECT_EQ((int)cuCimagf(outputHostData[1]), 1);
 }
 
+/*! \brief Simple test of 2 dimensional complex data
+ *  through a forward bfFFT.
+ */
 TEST(FFTTest, Handles2dComplex)
 {
-    BFarray my_data;
-    BFcomplex set_data[3][3] = 
+    /// Set up the test with a 3x3 element complex array
+    BFarray dataAndDescription;
+    cufftComplex hostData[3][3] = 
         {{{5,1},{0,0},{100,0}},
         {{5,1},{30,0},{100,0}},
         {{30,0},{0,0},{10,1}}};
-    BFcomplex** some_data;
-    cudaMalloc((void**)&some_data, sizeof(BFcomplex)*9);
+    /// Load this array to the GPU
+    cufftComplex** deviceData;
+    cudaMalloc((void**)&deviceData, sizeof(cufftComplex)*9);
     cudaMemcpy(
-        some_data, set_data, 
-        sizeof(BFcomplex)*9, cudaMemcpyHostToDevice);
-    my_data.data = some_data;
-    my_data.space = BF_SPACE_CUDA;
-    my_data.shape[0] = 3;
-    my_data.shape[1] = 3;
-    my_data.dtype = 1;
-    my_data.ndim = 2;
-    my_data.strides[0] = 3*sizeof(BFcomplex);
-    my_data.strides[1] = sizeof(BFcomplex);
-    bfFFT(&my_data, &my_data, FFT_FORWARD);
-    cufftComplex localdata[3][3]={};
+        deviceData, hostData, 
+        sizeof(cufftComplex)*9, cudaMemcpyHostToDevice);
+    dataAndDescription.data = deviceData;
+    /// Describe our data
+    dataAndDescription.space = BF_SPACE_CUDA;
+    dataAndDescription.shape[0] = 3;
+    dataAndDescription.shape[1] = 3;
+    dataAndDescription.dtype = 1;
+    dataAndDescription.ndim = 2;
+    dataAndDescription.strides[0] = 3*sizeof(cufftComplex);
+    dataAndDescription.strides[1] = sizeof(cufftComplex);
+
+    /// Perform the forward FFT in place
+    bfFFT(&dataAndDescription, &dataAndDescription, FFT_FORWARD);
+    /// Dowload data back from the GPU
     cudaMemcpy(
-        localdata, (cufftComplex**)my_data.data, 
+        hostData, (cufftComplex**)dataAndDescription.data, 
         sizeof(cufftComplex)*9, cudaMemcpyDeviceToHost);
-    EXPECT_EQ((int)cuCimagf(localdata[2][2]), -125);
+
+    /// Did the FFT produce the correct result?
+    /// We check this only with one value.
+    EXPECT_EQ((int)cuCimagf(hostData[2][2]), -125);
 }
 
+/*! \brief Simple test of 1 dimensional complex data
+ *  through an inverse bfFFT.
+ */
 TEST(FFTTest, InverseC2C)
 {
-    BFarray my_data;
-    BFcomplex set_data[5] = {{0,-1},{0,0},{100,-100},{30,0},{-5,0}};
-    BFcomplex* some_data;
-    cudaMalloc((void**)&some_data, sizeof(BFcomplex)*5);
-    cudaMemcpy(some_data, set_data, sizeof(BFcomplex)*5, cudaMemcpyHostToDevice);
-    my_data.data = some_data;
-    my_data.space = BF_SPACE_CUDA;
-    my_data.shape[0] = 5;
-    my_data.dtype = 1;
-    my_data.ndim = 1;
-    my_data.strides[0] = sizeof(BFcomplex);
-    bfFFT(&my_data, &my_data, FFT_INVERSE);
-    cufftComplex localdata[5]={};
-    cudaMemcpy(localdata, (cufftComplex*)my_data.data, sizeof(cufftComplex)*5, cudaMemcpyDeviceToHost);
+    /// Set up the test with a 5 element complex array
+    BFarray dataAndDescription;
+    cufftComplex hostData[5] = {{0,-1},{0,0},{100,-100},{30,0},{-5,0}};
+    /// Load this array to the GPU
+    cufftComplex* deviceData;
+    cudaMalloc((void**)&deviceData, sizeof(cufftComplex)*5);
+    cudaMemcpy(deviceData, hostData, sizeof(cufftComplex)*5, cudaMemcpyHostToDevice);
+    dataAndDescription.data = deviceData;
+    /// Describe our data
+    dataAndDescription.space = BF_SPACE_CUDA;
+    dataAndDescription.shape[0] = 5;
+    dataAndDescription.dtype = 1;
+    dataAndDescription.ndim = 1;
+    dataAndDescription.strides[0] = sizeof(cufftComplex);
 
-    EXPECT_EQ((int)cuCrealf(localdata[3]),139);
-}
-
-TEST(FFTTest, Handles1dComplex)
-{
-    //Set up the test with a 5 element complex array
-    BFarray my_data;
-    BFcomplex set_data[5] = {{0,0},{30,0},{100,0},{30,0},{-5,0}};
-    BFcomplex* some_data;
-    cudaMalloc((void**)&some_data, sizeof(BFcomplex)*5);
-    cudaMemcpy(some_data, set_data, sizeof(BFcomplex)*5, cudaMemcpyHostToDevice);
-    my_data.data = some_data;
-    my_data.space = BF_SPACE_CUDA;
-    my_data.shape[0] = 5;
-    my_data.dtype = 1;
-    my_data.ndim = 1;
-    my_data.strides[0] = sizeof(BFcomplex);
-
-    //Perform the FFT.
-    bfFFT(&my_data, &my_data, FFT_FORWARD);
-    //Unload the data from the device
-    cufftComplex localdata[5]={};
+    /// Perform the inverse FFT in place 
+    bfFFT(&dataAndDescription, &dataAndDescription, FFT_INVERSE);
+    /// Download back from the GPU
     cudaMemcpy(
-        localdata, (cufftComplex*)my_data.data, 
+        hostData, (cufftComplex*)dataAndDescription.data, 
         sizeof(cufftComplex)*5, cudaMemcpyDeviceToHost);
 
-    //Assert that last imaginary value is about 74
-    EXPECT_EQ((int)cuCimagf(localdata[4]),(int)74.431946);
+    /// Did the FFT produce the correct answer?
+    EXPECT_EQ((int)cuCrealf(hostData[3]),139);
+}
+
+/*! \brief Simple test of 1 dimensional complex data
+ *  through a forward bfFFT.
+ */
+TEST(FFTTest, Handles1dComplex)
+{
+    /// Set up the test with a 5 element complex array
+    BFarray dataAndDescription;
+    cufftComplex hostData[5] = 
+        {{0,0},{30,0},{100,0},{30,0},{-5,0}};
+    /// Load this array to the GPU
+    cufftComplex* deviceData;
+    cudaMalloc((void**)&deviceData, sizeof(cufftComplex)*5);
+    cudaMemcpy(deviceData, hostData, sizeof(cufftComplex)*5, cudaMemcpyHostToDevice);
+    dataAndDescription.data = deviceData;
+    /// Describe our data
+    dataAndDescription.space = BF_SPACE_CUDA;
+    dataAndDescription.shape[0] = 5;
+    dataAndDescription.dtype = 1;
+    dataAndDescription.ndim = 1;
+    dataAndDescription.strides[0] = sizeof(cufftComplex);
+
+    /// Perform the forward FFT in place 
+    bfFFT(&dataAndDescription, &dataAndDescription, FFT_FORWARD);
+    /// Download back from the GPU.
+    cudaMemcpy(
+        hostData, (cufftComplex*)dataAndDescription.data, 
+        sizeof(cufftComplex)*5, cudaMemcpyDeviceToHost);
+
+    /// Test a single value of the output, to test for
+    /// the FFT's correct answer?
+    EXPECT_EQ((int)cuCimagf(hostData[4]),(int)74.431946);
 }
 
 
