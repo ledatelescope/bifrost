8aefae4cb54d8fc139abeba282a97f0ff6dcd286
mdtmc <miles.cranmer@gmail.com>
2016-07-03 11:12:25 -0700

    Add sigproc to bifrost lib

--- python/bifrost/sigproc.py
+++ python/bifrost/sigproc.py
@@ -1,3 +1,4 @@
+
 # Copyright (c) 2016, The Bifrost Authors. All rights reserved.
 # Copyright (c) 2016, NVIDIA CORPORATION. All rights reserved.
 #
@@ -26,21 +27,6 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #This program is for reading and writing sigproc filterbank files.
-
-#  Copyright 2015 Ben Barsdell
-#  Copyright 2016 NVIDIA Corporation
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
 
 """
 telescope_id:  0 (FAKE)
