0e0ad221c0cbdd51c106dc520e946a6ba84767a8
mdtmc <miles.cranmer@gmail.com>
2016-07-05 16:08:27 -0700

    Confirmed dada block works with copy block

--- python/bifrost/test_block.py
+++ python/bifrost/test_block.py
@@ -124,6 +124,15 @@ class TestDadaBlock(unittest.TestCase):
         Pipeline(self.blocks).main() 
         test_bytes = open(logfile, 'r').read(500).split(' ')
         self.assertAlmostEqual(np.float(test_bytes[0]), 3908.5, 3)
+    def test_read_copy_write(self):
+        """Adds another intermediate block to the
+            last step."""
+        logfile = '.log.txt'
+        self.blocks.append((CopyBlock(), [0], [1, 2, 3]))
+        self.blocks.append((WriteAsciiBlock(logfile), [3], []))
+        Pipeline(self.blocks).main() 
+        test_bytes = open(logfile, 'r').read(500).split(' ')
+        self.assertAlmostEqual(np.float(test_bytes[0]), 3908.5, 3)
 
 if __name__ == "__main__":
     unittest.main()
