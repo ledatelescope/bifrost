00948a909c7ad1ea199881c25ca2f8756c57b4bd
mdtmc <miles.cranmer@gmail.com>
2016-07-08 09:17:51 -0700

    Added jenkins python jail

--- jenkins.sh
+++ jenkins.sh
@@ -7,8 +7,11 @@ while [ "$1" != "" ]; do
             INSTALL_LIB_DIR=$JENKINS_HOME/usr/local/lib \
                 INSTALL_INC_DIR=$JENKINS_HOME/usr/local/include \
                 NVCC=/usr/local/cuda-6.5/bin/nvcc make -j
+            cd python
+            python setup.py install --prefix=$JENKINS_HOME/usr/local
             ;;
         --test )
+            export PYTHONPATH=$JENKINS_HOME/usr/local/lib/python2.7
             cd test;
             python *.py
             ;;
