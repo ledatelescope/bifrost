c5edb89595dabbbb31eb00c37a0c03f874bfef93
mdtmc <miles.cranmer@gmail.com>
2016-07-08 08:59:00 -0700

    Added testing for jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,20 @@
-# Script for the Jenkins build system
 #!/bin/bash
-export PYTHONPATH=/data1/mcranmer/usr/lib/python2.7/site-packages
-INSTALL_LIB_DIR=$JENKINS_HOME/usr/local/lib INSTALL_INC_DIR=$JENKINS_HOME/usr/local/include NVCC=/usr/local/cuda-6.5/bin/nvcc make -j
+# Script for the Jenkins build system
+
+export PYTHONPATH=$PYTHONPATH:/data1/mcranmer/usr/lib/python2.7/site-packages
+while [ "$1" != "" ]; do 
+    case $1 in
+        --build )
+            INSTALL_LIB_DIR=$JENKINS_HOME/usr/local/lib \
+                INSTALL_INC_DIR=$JENKINS_HOME/usr/local/include \
+                NVCC=/usr/local/cuda-6.5/bin/nvcc make -j
+            ;;
+        --test )
+            cd test;
+            python *.py
+            ;;
+        * )
+            exit 1
+    esac
+    shift
+done
