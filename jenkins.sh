88eb74f8ea522611e576acb1781ba420bef8656d
mdtmc <miles.cranmer@gmail.com>
2016-07-08 06:41:31 -0700

    Fixed initialization syntax

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,4 @@
 # Script for the Jenkins build system
-!#/bin/bash
+#!/bin/bash
 export PYTHONPATH=/data1/mcranmer/usr/lib/python2.7/site-packages
 INSTALL_LIB_DIR=$JENKINS_HOME/usr/local/lib INSTALL_INC_DIR=$JENKINS_HOME/usr/local/include NVCC=/usr/local/cuda-6.5/bin/nvcc make -j
