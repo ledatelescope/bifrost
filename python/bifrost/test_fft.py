692293ded36fa22fc26d64fae56af2d60987392a
mdtmc <miles.cranmer@gmail.com>
2016-07-18 10:00:30 -0700

    Added simple fft block for system memory

---
+++ python/bifrost/test_fft.py
@@ -0,0 +1,30 @@
+import unittest
+from block import *
+
+class TestFFTBlock(unittest.TestCase):
+    """This test assures basic functionality of fft block"""
+    def setUp(self):
+        """Assemble a basic pipeline with the FFT block"""
+        self.logfile = '.log.txt'
+        self.blocks = []
+        self.blocks.append((
+            SigprocReadBlock(
+                '/data1/mcranmer/data/fake/1chan8bitNoDM.fil'),
+            [], [0]))
+        self.blocks.append((FFTBlock(gulp_size=4096*8*8*8), [0], [1]))
+        self.blocks.append((WriteAsciiBlock(self.logfile), [1], []))
+    def test_throughput(self):
+        """Test that any data is being put through"""
+        Pipeline(self.blocks).main()
+        test_string = open(self.logfile, 'r').read()
+        self.assertGreater(len(test_string), 0)
+    def test_throughput_size(self):
+        """Number of elements going out should be double that of basic copy"""
+        Pipeline(self.blocks).main()
+        number_fftd = len(open(self.logfile, 'r').read().split(' '))
+        open(self.logfile, 'w').close()
+        ## Rune pipeline again with simple copy
+        self.blocks[1] = (CopyBlock(), [0], [1])
+        Pipeline(self.blocks).main()
+        number_copied = len(open(self.logfile, 'r').read().split(' '))
+        self.assertEqual(number_fftd, 2*number_copied)
