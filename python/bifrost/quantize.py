44e511638ad2bec1eaa95c7969adf8ad776b12e6
Miles Cranmer <miles.cranmer@gmail.com>
2017-01-14 20:16:56 -0500

    Removing trailing comma

--- python/bifrost/quantize.py
+++ python/bifrost/quantize.py
@@ -26,7 +26,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from libbifrost import _bf, _check, _get,
+from libbifrost import _bf, _check, _get
 from array import _array2bifrost
 
 import ctypes
