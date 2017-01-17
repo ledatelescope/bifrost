7386b6aab2eb3add73b316d46b5288276793db15
mdtmc <miles.cranmer@gmail.com>
2017-01-17 12:43:00 -0500

    Added more python tests to travis

--- test/travis.sh
+++ test/travis.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This file runs CPU-safe tests for travis-ci
 ./download_test_data.sh
-python -m unittest test_block \
-&& python -m unittest test_sigproc
+python -m unittest test_block &&
+python -m unittest test_sigproc &&
+python -m unittest test_resizing 
