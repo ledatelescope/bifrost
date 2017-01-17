f24d2a6f9a813f5c23120421b3d8bd1e1103f1f4
Miles Cranmer <miles.cranmer@gmail.com>
2017-01-17 18:36:15 -0500

    More tests for travis

--- test/travis.sh
+++ test/travis.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # This file runs CPU-safe tests for travis-ci
 ./download_test_data.sh
-python -m unittest test_block &&
-python -m unittest test_sigproc &&
-python -m unittest test_resizing 
+python -m unittest test_block test_sigproc test_resizing test_quantize test_unpack
