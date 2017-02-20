3bcab337888369e176a00360f7f6509754cf5474
Ben Barsdell <benbarsdell@gmail.com>
2017-02-20 00:29:59 -0800

    Added audio source block

---
+++ python/bifrost/audio_block.py
@@ -0,0 +1,66 @@
+
+# Copyright (c) 2016, The Bifrost Authors. All rights reserved.
+# Copyright (c) 2016, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+# * Neither the name of The Bifrost Authors nor the names of its
+#   contributors may be used to endorse or promote products derived
+#   from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from pipeline import SourceBlock
+import portaudio as audio
+
+class AudioSourceBlock(SourceBlock):
+	def create_reader(self, kwargs):
+		self.reader = audio.open(mode='r', **kwargs)
+		return self.reader
+	def on_sequence(self, reader, kwargs):
+		if 'frames_per_buffer' not in kwargs:
+			kwargs['frames_per_buffer'] = self.gulp_nframe
+		ohdr = {
+			'_tensor': {
+				'dtype':  'i' + str(reader.nbits),
+				'shape':  [-1, reader.channels],
+				'labels': ['time', 'channel'], # TODO: 'channel' vs. 'polarisation'?
+				'scales': [1./reader.rate, None],
+				'units':  ['s', None]
+			},
+			'frame_rate':   reader.rate,
+			'input_device': reader.input_device,
+			'name': str(id(reader))
+		}
+		return [ohdr]
+	def on_data(self, reader, ospans):
+		ospan = ospans[0]
+		try:
+			reader.readinto(ospan.data)
+		except audio.PortAudioError:
+			#raise StopIteration
+			return [0]
+		nframe = ospan.shape[0]
+		print nframe
+		return [nframe]
+	def stop(self):
+		self.reader.stop()
+
+def read_audio(audio_kwargs, gulp_nframe, *args, **kwargs):
+	return AudioSourceBlock(audio_kwargs, gulp_nframe, *args, **kwargs)#.orings[0]
