4efde04751f47164554723ee6657bd003984229d
Ben Barsdell <benbarsdell@gmail.com>
2017-02-26 22:17:57 -0800

    Added accumulate block (sums input frames one at a time)

--- python/bifrost/blocks/accumulate.py
+++ python/bifrost/blocks/accumulate.py
@@ -26,19 +26,54 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+"""
+A simple block that accepts 1 frame at a time and accumulates them
+nframe times before outputting the accumulated result.
+"""
+
 from __future__ import absolute_import
 
-from .copy import copy, CopyBlock
-from .transpose import transpose, TransposeBlock
-from .fft import fft, FftBlock
-from .fftshift import fftshift, FftShiftBlock
-from .fdmt import fdmt, FdmtBlock
-from .detect import detect, DetectBlock
-from .guppi_raw import read_guppi_raw, GuppiRawSourceBlock
-from .sigproc import read_sigproc, SigprocSourceBlock
-from .scrunch import scrunch, ScrunchBlock
-from .accumulate import accumulate, AccumulateBlock
-try: # Avoid error if portaudio library not installed
-	from .audio import read_audio, AudioSourceBlock
-except:
-	pass
+import bifrost as bf
+from bifrost.pipeline import TransformBlock
+
+from copy import deepcopy
+
+class AccumulateBlock(TransformBlock):
+	def __init__(self, iring, nframe, dtype=None, gulp_nframe=1,
+	             *args, **kwargs):
+		assert(gulp_nframe == 1)
+		super(AccumulateBlock, self).__init__(iring, gulp_nframe=1,
+		                                      *args, **kwargs)
+		self.nframe = nframe
+		self.dtype  = dtype
+	def define_valid_input_spaces(self):
+		"""Return set of valid spaces (or 'any') for each input"""
+		return ('cuda',)
+	def on_sequence(self, iseq):
+		ihdr = iseq.header
+		itensor = ihdr['_tensor']
+		ohdr = deepcopy(ihdr)
+		otensor = ohdr['_tensor']
+		if 'scales' in otensor:
+			frame_axis = otensor['shape'].index(-1)
+			otensor['scales'][frame_axis][1] *= self.nframe
+		if self.dtype is not None:
+			otensor['dtype'] = self.dtype
+		self.frame_count = 0
+		return ohdr
+	def on_data(self, ispan, ospan):
+		print "accumulate::on_data", ispan.data.shape, ospan.data.shape
+		idata = ispan.data
+		odata = ospan.data
+		beta = 0. if self.frame_count == 0 else 1.
+		bf.map("b = beta * b + (b_type)a", a=idata, b=odata, beta=beta)
+		self.frame_count += 1
+		if self.frame_count == self.nframe:
+			ncommit = 1
+			self.frame_count = 0
+		else:
+			ncommit = 0
+		return ncommit
+
+def accumulate(iring, nframe, *args, **kwargs):
+	return AccumulateBlock(iring, nframe, *args, **kwargs)
