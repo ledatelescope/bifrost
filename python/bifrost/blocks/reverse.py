daeb0e4831f166f88a7cf8346f031ed6fbff55ed
Ben Barsdell <benbarsdell@gmail.com>
2017-03-14 20:19:07 -0700

    Added ReverseBlock

--- python/bifrost/blocks/reverse.py
+++ python/bifrost/blocks/reverse.py
@@ -28,20 +28,51 @@
 
 from __future__ import absolute_import
 
-from .copy import copy, CopyBlock
-from .transpose import transpose, TransposeBlock
-from .reverse import reverse, ReverseBlock
-from .fft import fft, FftBlock
-from .fftshift import fftshift, FftShiftBlock
-from .fdmt import fdmt, FdmtBlock
-from .detect import detect, DetectBlock
-from .guppi_raw import read_guppi_raw, GuppiRawSourceBlock
-from .sigproc import read_sigproc, SigprocSourceBlock
-from .scrunch import scrunch, ScrunchBlock
-from .accumulate import accumulate, AccumulateBlock
-from .unpack import unpack, UnpackBlock
-from .quantize import quantize, QuantizeBlock
-try: # Avoid error if portaudio library not installed
-	from .audio import read_audio, AudioSourceBlock
-except:
-	pass
+import bifrost as bf
+from bifrost.pipeline import TransformBlock
+from bifrost.DataType import DataType
+
+from copy import deepcopy
+
+class ReverseBlock(TransformBlock):
+	def __init__(self, iring, axes, *args, **kwargs):
+		super(ReverseBlock, self).__init__(iring, *args, **kwargs)
+		if not isinstance(axes, list) or isinstance(axes, tuple):
+			axes = [axes]
+		self.specified_axes = axes
+	def define_valid_input_spaces(self):
+		"""Return set of valid spaces (or 'any') for each input"""
+		return ('cuda',)
+	def on_sequence(self, iseq):
+		ihdr = iseq.header
+		itensor = ihdr['_tensor']
+		self.axes = [itensor['labels'].index(axis)
+		             if isinstance(axis, basestring)
+		             else axis
+		             for axis in self.specified_axes]
+		frame_axis = itensor['shape'].index(-1)
+		if frame_axis in self.axes:
+			raise KeyError("Cannot reverse frame axis")
+		ohdr = deepcopy(ihdr)
+		otensor = ohdr['_tensor']
+		oshape = otensor['shape']
+		if 'scales' in itensor:
+			for ax in self.axes:
+				scale_step = otensor['scales'][ax][1]
+				scale_shift = oshape[ax] * scale_step
+				otensor['scales'][ax][0] += scale_shift
+				otensor['scales'][ax][1]  = -scale_step
+		return ohdr
+	def on_data(self, ispan, ospan):
+		idata = ispan.data
+		odata = ospan.data
+		shape = idata.shape
+		ind_names = ['i%i'%i for i in xrange(idata.ndim)]
+		inds = list(ind_names)
+		for ax in self.axes:
+			inds[ax] = '-'+inds[ax]
+		inds = ','.join(inds)
+		bf.map("b = a(%s)" % inds, shape, *ind_names, a=idata, b=odata)
+
+def reverse(iring, axes, *args, **kwargs):
+	return ReverseBlock(iring, axes, *args, **kwargs)
