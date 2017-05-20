610ee2706b3087d708d32e80cb2b12d488c1e623
MilesCranmer <miles.cranmer@gmail.com>
2017-05-20 04:19:41 -0700

    Fix tabs in quantize and guppi raw

--- python/bifrost/blocks/quantize.py
+++ python/bifrost/blocks/quantize.py
@@ -36,31 +36,31 @@ from bifrost.DataType import DataType
 from copy import deepcopy
 
 class QuantizeBlock(TransformBlock):
-	def __init__(self, iring, dtype, scale=1.,
-	             *args, **kwargs):
-		super(QuantizeBlock, self).__init__(iring, *args, **kwargs)
-		self.dtype = dtype
-		self.scale = scale
-	def define_valid_input_spaces(self):
-		"""Return set of valid spaces (or 'any') for each input"""
-		return ('system',)
-	def on_sequence(self, iseq):
-		ihdr = iseq.header
-		ohdr = deepcopy(ihdr)
-		itype = DataType(ihdr['_tensor']['dtype'])
-		self.itype = itype
-		# Allow user to pass nbit instead of explicit dtype
-		if isinstance(self.dtype, int):
-			nbit = self.dtype
-			otype = itype.as_integer(nbit)
-		else:
-			otype = self.dtype
-		ohdr['_tensor']['dtype'] = otype
-		return ohdr
-	def on_data(self, ispan, ospan):
-		idata = ispan.data
-		odata = ospan.data
-		bf.quantize.quantize(idata, odata, self.scale)
+    def __init__(self, iring, dtype, scale=1.,
+                 *args, **kwargs):
+        super(QuantizeBlock, self).__init__(iring, *args, **kwargs)
+        self.dtype = dtype
+        self.scale = scale
+    def define_valid_input_spaces(self):
+        """Return set of valid spaces (or 'any') for each input"""
+        return ('system',)
+    def on_sequence(self, iseq):
+        ihdr = iseq.header
+        ohdr = deepcopy(ihdr)
+        itype = DataType(ihdr['_tensor']['dtype'])
+        self.itype = itype
+        # Allow user to pass nbit instead of explicit dtype
+        if isinstance(self.dtype, int):
+            nbit = self.dtype
+            otype = itype.as_integer(nbit)
+        else:
+            otype = self.dtype
+        ohdr['_tensor']['dtype'] = otype
+        return ohdr
+    def on_data(self, ispan, ospan):
+        idata = ispan.data
+        odata = ospan.data
+        bf.quantize.quantize(idata, odata, self.scale)
 
 def quantize(iring, dtype, *args, **kwargs):
     """Apply a requantization of bit depth for the data.
@@ -88,4 +88,4 @@ def quantize(iring, dtype, *args, **kwargs):
     -------
     `QuantizeBlock`
     """
-	return QuantizeBlock(iring, dtype, *args, **kwargs)
+    return QuantizeBlock(iring, dtype, *args, **kwargs)
