0df097adf0d5ee4a9bd00a73006049ff7abf8693
Danny Price <dan@thetelegraphic.com>
2017-05-24 10:16:52 -0700

    Revert "Trying out sphinx + github doc building"
    This reverts commit 34ed24389542c95ae7574f9efbfc525d248fc8ef.
    
    Removing HTML files from /docs and Miles has a different approach
    that is more suitable.

--- docs/generate_python_reference.sh
+++ docs/generate_python_reference.sh
@@ -4,8 +4,3 @@ sed -i '1s/.*/Python Reference/' source/bifrost.rst
 sed -i '2s/.*/================/' source/bifrost.rst
 sed -i '1s/.*/Block Library Reference/' source/bifrost.blocks.rst
 sed -i '2s/.*/=======================/' source/bifrost.blocks.rst
-make html
-rm -rf _modules
-rm -rf _static
-rm -rf _sources
-mv build/html/* ./
