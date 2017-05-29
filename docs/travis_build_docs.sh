6cfb3d81611b9ef30c4de53b602055cd6bee31ba
MilesCranmer <miles.cranmer@gmail.com>
2017-05-29 11:57:42 +1000

    Fixed travis docs builder

--- docs/travis_build_docs.sh
+++ docs/travis_build_docs.sh
@@ -1,3 +1,3 @@
 docker build -t bifrost_docs -f Dockerfile .
 mkdir -p out
-docker run --rm -i --net=none -v $(pwd)/out:/data /bin/bash -c "cp -r ./* /data/"
+docker run --rm -i --net=none -v $(pwd)/out:/data bifrost_docs /bin/bash -c "cp -r ./* /data/"
