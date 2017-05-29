4870f257ec8f3d802f3448279601551e7db947fd
MilesCranmer <miles.cranmer@gmail.com>
2017-05-29 11:55:31 +1000

    Remove redundant docs generation
    - Already inside makefile

--- docs/generate_cpp_reference.sh
+++
@@ -1,2 +0,0 @@
-breathe-apidoc -o source -p bifrost --force ./doxygen/xml/
-rm -rf source/file
--- docs/generate_python_reference.sh
+++
@@ -1,6 +0,0 @@
-sphinx-apidoc -o source -d 5 --force ../python/bifrost/
-rm source/modules.rst
-sed -i '1s/.*/Python Reference/' source/bifrost.rst
-sed -i '2s/.*/================/' source/bifrost.rst
-sed -i '1s/.*/Block Library Reference/' source/bifrost.blocks.rst
-sed -i '2s/.*/=======================/' source/bifrost.blocks.rst
