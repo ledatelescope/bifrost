75408273407f46bc90f0c535d7b4ddce6d15f97b
MilesCranmer <miles.cranmer@gmail.com>
2017-06-09 11:06:56 +0000

    Refactor bifrost_benchmarks as new module
    - Sigproc pipeline benchmark file now creates a class which inherits from the PipelineBenchmarker class. This provides similar functionality for timing future pipelines

---
+++ test/benchmarks/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""Installation script for some shared benchmarking utilities
+"""
+
+from setuptools import setup
+
+setup(name='bifrost_benchmarks',
+      version='0.0',
+      description='Shared benchmarking utility',
+      url='github.com/ledatelescope/bifrost',
+      packages=['bifrost_benchmarks'],
+      license='MIT',
+      package_dir={'bifrost_benchmarks':'bifrost_benchmarks'},
+      install_requires=[
+          'bifrost',
+      ])
