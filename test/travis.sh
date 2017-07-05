272ecfbe30b5fb500e2241bc3a2bb23237b925e7
MilesCranmer <miles.cranmer@gmail.com>
2017-07-05 11:41:26 -0400

    travis: fix unittest quirk with manual source additions

--- test/travis.sh
+++ test/travis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This file runs CPU-safe tests for travis-ci
 ./download_test_data.sh
-coverage run --source=bifrost -m unittest test_block test_sigproc test_resizing test_quantize test_unpack test_print_header
+coverage run --source=bifrost.ring,bifrost,bifrost.pipeline -m unittest test_block test_sigproc test_resizing test_quantize test_unpack test_print_header
