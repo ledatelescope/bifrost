613082e67b4d1781f29f83a3746481516bfe13b7
Ben Barsdell <benbarsdell@gmail.com>
2017-07-08 11:10:48 -0700

    Add tests for SerializeBlock

--- test/travis.sh
+++ test/travis.sh
@@ -2,4 +2,11 @@
 # This file runs CPU-safe tests for travis-ci
 ./download_test_data.sh
 export LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
-coverage run --source=bifrost.ring,bifrost,bifrost.pipeline -m unittest test_block test_sigproc test_resizing test_quantize test_unpack test_print_header
+coverage run --source=bifrost.ring,bifrost,bifrost.pipeline -m unittest \
+  test_block \
+  test_sigproc \
+  test_resizing \
+  test_quantize \
+  test_unpack \
+  test_print_header \
+  test_serialize
