3cca2e9636b5fa74d5ed0578386f815001f03451
Ben Barsdell <benbarsdell@gmail.com>
2017-07-08 00:36:09 -0700

    Add batching support to FDMT, and add more tests
    - Supports any number of batch dims as long as they are contiguous.
    - The tests are still not very good; need to find a convenient way to
      check the actual values.

--- test/test_fdmt.py
+++ test/test_fdmt.py
@@ -32,33 +32,70 @@ import bifrost as bf
 from bifrost.fdmt import Fdmt
 
 class FdmtTest(unittest.TestCase):
-    def test_fdmt(self):
+    def run_test(self, ntime, nchan, max_delay, batch_shape=()):
         fdmt = Fdmt()
-        ntime     = 1024
-        nchan     = 128
-        max_delay = 200
         f0        = 1000.
         bw        = 400.
         df        = bw / nchan
         exponent  = -2.0
         fdmt.init(nchan, max_delay, f0, df, exponent, 'cuda')
-        idata = bf.asarray(np.random.normal(size=(nchan, ntime))
+        ishape = batch_shape + (nchan, ntime)
+        oshape = batch_shape + (max_delay, ntime)
+        idata = bf.asarray(np.random.normal(size=ishape)
                            .astype(np.float32), space='cuda')
 
-        odata1 = bf.asarray(-999 * np.ones((max_delay, ntime), np.float32),
+        odata1 = bf.asarray(-999 * np.ones(oshape, np.float32),
                             space='cuda')
         fdmt.execute(idata, odata1)
         odata1 = odata1.copy('system')
-        self.assertEqual(odata1.min(), -999)
+        if max_delay > 1:
+            self.assertEqual(odata1.min(), -999)
         # TODO: Need better tests
         self.assertLess(odata1.max(), 100.)
 
-        odata2 = bf.asarray(-999 * np.ones((max_delay, ntime), np.float32),
+        odata2 = bf.asarray(-999 * np.ones(oshape, np.float32),
                             space='cuda')
         workspace_size = fdmt.get_workspace_size(idata, odata2)
-        self.assertEqual(workspace_size, 3293184)
         workspace = bf.asarray(np.empty(workspace_size, np.uint8), space='cuda')
         workspace_ptr = workspace.ctypes.data
         fdmt.execute_workspace(idata, odata2, workspace_ptr, workspace_size)
         odata2 = odata2.copy('system')
         np.testing.assert_equal(odata1, odata2)
+    def test_ntime1024_nchan128_ndelay200(self):
+        self.run_test(ntime=1024, nchan=128, max_delay=200)
+    def test_ntime1024_nchan2_ndelay200(self):
+        self.run_test(ntime=1024, nchan=2, max_delay=20)
+    def test_ntime1024_nchan200_ndelay2(self):
+        self.run_test(ntime=1024, nchan=32, max_delay=2)
+    def test_ntime1024_nchan32_ndelay1(self):
+        self.run_test(ntime=1024, nchan=32, max_delay=1)
+    def test_ntime1024_nchan33_ndelay65(self):
+        self.run_test(ntime=1024, nchan=33, max_delay=65)
+    def test_ntime17_nchan33_ndelay65(self):
+        self.run_test(ntime=17, nchan=33, max_delay=65)
+
+    def test_ntime1024_nchan128_ndelay200_batch7(self):
+        self.run_test(ntime=1024, nchan=128, max_delay=200, batch_shape=(7,))
+    def test_ntime1024_nchan2_ndelay200_batch7(self):
+        self.run_test(ntime=1024, nchan=2, max_delay=20, batch_shape=(7,))
+    def test_ntime1024_nchan200_ndelay2_batch7(self):
+        self.run_test(ntime=1024, nchan=32, max_delay=2, batch_shape=(7,))
+    def test_ntime1024_nchan32_ndelay1_batch7(self):
+        self.run_test(ntime=1024, nchan=32, max_delay=1, batch_shape=(7,))
+    def test_ntime1024_nchan33_ndelay65_batch7(self):
+        self.run_test(ntime=1024, nchan=33, max_delay=65, batch_shape=(7,))
+    def test_ntime17_nchan33_ndelay65_batch7(self):
+        self.run_test(ntime=17, nchan=33, max_delay=65, batch_shape=(7,))
+
+    def test_ntime1024_nchan128_ndelay200_batch7_9_5(self):
+        self.run_test(ntime=1024, nchan=128, max_delay=200, batch_shape=(7,9,5))
+    def test_ntime1024_nchan2_ndelay200_batch7_9_5(self):
+        self.run_test(ntime=1024, nchan=2, max_delay=20, batch_shape=(7,9,5))
+    def test_ntime1024_nchan200_ndelay2_batch7_9_5(self):
+        self.run_test(ntime=1024, nchan=32, max_delay=2, batch_shape=(7,9,5))
+    def test_ntime1024_nchan32_ndelay1_batch7_9_5(self):
+        self.run_test(ntime=1024, nchan=32, max_delay=1, batch_shape=(7,9,5))
+    def test_ntime1024_nchan33_ndelay65_batch7_9_5(self):
+        self.run_test(ntime=1024, nchan=33, max_delay=65, batch_shape=(7,9,5))
+    def test_ntime17_nchan33_ndelay65_batch7_9_5(self):
+        self.run_test(ntime=17, nchan=33, max_delay=65, batch_shape=(7,9,5))
