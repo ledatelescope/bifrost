fd2e47adfa0fcf6f7e410a7e6e913ed9d6bbc1c6
MilesCranmer <miles.cranmer@gmail.com>
2017-07-17 19:08:35 +0000

    Remove tests from binary_io.py itself
    - These are now unittests in test/test_binary_io.py

--- python/bifrost/blocks/binary_io.py
+++ python/bifrost/blocks/binary_io.py
@@ -129,34 +129,3 @@ class BinaryFileWriteBlock(bfp.SinkBlock):
 
     def on_data(self, ispan):
         self.current_fileobj.write(ispan.data.tobytes())
-
-if __name__ == "__main__":
-
-    # Generate test vector and save to file
-    t = np.arange(32768 * 1024)
-    w = 0.01
-    s0 = np.sin(w * t, dtype='float32')
-    s0.tofile('numpy_data0.bin')
-    s1 = np.sin(w * 4 * t, dtype='float32')
-    s1.tofile('numpy_data1.bin')
-
-    # Setup pipeline
-    filenames   = ['numpy_data0.bin', 'numpy_data1.bin']
-    b_read      = BinaryFileReadBlock(filenames, 32768, 1, 'f32')
-    b_write     = BinaryFileWriteBlock(b_read.orings[0])
-
-    # Run pipeline
-    pipeline = bfp.get_default_pipeline()
-    pipeline.run()
-
-    # Check the output files match the input files
-    outdata0 = np.fromfile('numpy_data0.bin.out', dtype='float32')
-    outdata1 = np.fromfile('numpy_data1.bin.out', dtype='float32')
-
-    try:
-        assert np.allclose(s0, outdata0)
-        assert np.allclose(s1, outdata1)
-        print "Input data and output data match."
-    except AssertionError:
-        print "Error: input and output data do not match."
-        raise
