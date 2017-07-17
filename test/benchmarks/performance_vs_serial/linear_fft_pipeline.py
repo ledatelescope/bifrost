47c38b660e9cfb80d34bb59c3033275cd21469e7
MilesCranmer <miles.cranmer@gmail.com>
2017-07-17 18:39:39 +0000

    Generate binary data for linear_fft_pipeline

--- test/benchmarks/performance_vs_serial/linear_fft_pipeline.py
+++ test/benchmarks/performance_vs_serial/linear_fft_pipeline.py
@@ -1,5 +1,6 @@
 """ Test a pipeline with repeated FFTs and inverse FFTs """
 from timeit import default_timer as timer
+import numpy as np
 import bifrost as bf
 from bifrost import pipeline as bfp
 from bifrost import blocks as blocks
@@ -9,9 +10,10 @@ class GPUFFTBenchmarker(PipelineBenchmarker):
     """ Test the sigproc read function """
     def run_benchmark(self):
         with bf.Pipeline() as pipeline:
-            fil_file = "../../../data/1chan8bitNoDM.fil"
-            data = blocks.read_sigproc([fil_file], gulp_nframe=4096)
-            data.on_data = self.timeit(data.on_data)
+            datafile = "numpy_data0.bin"
+            data = blocks.binary_io.BinaryFileReadBlock(
+                    [datafile], gulp_size=32768, gulp_nframe=4, dtype='f32')
+            #data.on_data = self.timeit(data.on_data)
 
             start = timer()
             pipeline.run()
@@ -21,3 +23,10 @@ class GPUFFTBenchmarker(PipelineBenchmarker):
 #sigproc_benchmarker = SigprocBenchmarker()
 #print sigproc_benchmarker.average_benchmark(10)
 
+t = np.arange(32768*1024)
+w = 0.01
+s = np.sin(w * 4 * t, dtype='float32')
+with open('numpy_data0.bin', 'wb') as myfile: pass
+s.tofile('numpy_data0.bin')
+gpufftbenchmarker = GPUFFTBenchmarker()
+print gpufftbenchmarker.average_benchmark(10)
