983ddc8d47076d46c31193588cc3ca6c4e894c02
MilesCranmer <miles.cranmer@gmail.com>
2017-07-18 20:41:38 +0000

    Added comparison with CPU numpy pipeline
    - Bifrost finishes in 0.97 seconds, numpy in about 86.7 seconds

--- test/benchmarks/performance_vs_serial/linear_fft_pipeline.py
+++ test/benchmarks/performance_vs_serial/linear_fft_pipeline.py
@@ -21,12 +21,22 @@ class GPUFFTBenchmarker(PipelineBenchmarker):
             for _ in range(NUMBER_FFT):
                 bc.blocks.fft(['gulped'], axis_labels=['ft_gulped'])
                 bc.blocks.fft(['ft_gulped'], axis_labels=['gulped'], inverse=True)
-            bc.blocks.print_header()
 
             start = timer()
             pipeline.run()
             end = timer()
             self.total_clock_time = end-start
+
+def regular_numpy_fft_pipeline(filename):
+    data = []
+    start = timer()
+    with open(filename, 'r') as file_obj:
+        data = np.fromfile(file_obj, dtype=np.float32)
+    for _ in range(NUMBER_FFT):
+        data = np.fft.fft(data)
+        data = np.fft.ifft(data)
+    end = timer()
+    return end-start
 
 t = np.arange(32768*1024)
 w = 0.01
@@ -34,4 +44,6 @@ s = np.sin(w * 4 * t, dtype='float32')
 with open('numpy_data0.bin', 'wb') as myfile: pass
 s.tofile('numpy_data0.bin')
 gpufftbenchmarker = GPUFFTBenchmarker()
-print gpufftbenchmarker.average_benchmark(4)
+print "Bifrost gets:", gpufftbenchmarker.average_benchmark(4)[0]
+
+print "Regular single-threaded numpy gets:", regular_numpy_fft_pipeline('numpy_data0.bin')
