bb4272ea56a0251f49bdda0f20a3002fbb28a6e0
MilesCranmer <miles.cranmer@gmail.com>
2017-07-20 19:23:25 -0400

    Bind copy block to separate core for benchmark

--- test/benchmarks/performance_vs_serial/linear_fft_pipeline.py
+++ test/benchmarks/performance_vs_serial/linear_fft_pipeline.py
@@ -11,7 +11,7 @@ NUMBER_FFT = int(os.environ['NUMBER_FFT'])
 SIZE_MULTIPLIER = int(os.environ['SIZE_MULTIPLIER'])
 GULP_SIZE = int(os.environ['GULP_SIZE'])
 CORES = int(os.environ['BF_CORES'])
-assert(CORES > 1)
+assert(CORES > 2)
 
 class GPUFFTBenchmarker(PipelineBenchmarker):
     """ Test the sigproc read function """
@@ -22,15 +22,15 @@ class GPUFFTBenchmarker(PipelineBenchmarker):
             bc = bf.BlockChainer()
             bc.blocks.binary_read(
                     [datafile], gulp_size=GULP_SIZE, gulp_nframe=1, dtype='f32', core=CORES-1)
-            bc.blocks.copy('cuda')
+            bc.blocks.copy('cuda', core=CORES-2)
             cur_core = 0
             for _ in range(NUMBER_FFT):
                 bc.blocks.fft(['gulped'], axis_labels=['ft_gulped'], core=cur_core)
                 cur_core += 1
-                cur_core = cur_core % (CORES - 1)
+                cur_core = cur_core % (CORES - 2)
                 bc.blocks.fft(['ft_gulped'], axis_labels=['gulped'], inverse=True, core=cur_core)
                 cur_core += 1
-                cur_core = cur_core % (CORES - 1)
+                cur_core = cur_core % (CORES - 2)
 
             start = timer()
             pipeline.run()
