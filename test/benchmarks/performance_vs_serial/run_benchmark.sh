166486863ee028c7948b855311ea0827ad684d3a
MilesCranmer <miles.cranmer@gmail.com>
2017-07-21 17:42:54 +0000

    Improve formatting output of benchmarks runner
    - Now is output as a csv file
    - The >>>START and END<<< keys are necessary for when commands produce
      errors - they give an easy way to clean up after.

--- test/benchmarks/performance_vs_serial/run_benchmark.sh
+++ test/benchmarks/performance_vs_serial/run_benchmark.sh
@@ -4,6 +4,8 @@ declare -a fft_array=("1" "2")
 declare -a size_array=("2" "4" "8" "16")
 declare -a gulp_array=("1" "2" "4" "8" "16" "32" "64" "128")
 
+echo "start key, # FFT's/2, size multiplier, gulp size, bifrost execution time, skcuda execution time, speedup, end key"
+
 for i in "${fft_array[@]}"
 do
 
@@ -13,18 +15,15 @@ do
 for k in "${gulp_array[@]}"
 do
 
-echo "STARTING NEW BENCHMARK"
+echo -n ">>>START, "
 export NUMBER_FFT="$i"
 export SIZE_MULTIPLIER="$j"
 export GULP_SIZE="$(echo "32768*1024/$k" | bc)"
-echo "$NUMBER_FFT, $SIZE_MULTIPLIER, $GULP_SIZE"
+echo -n "$NUMBER_FFT, $SIZE_MULTIPLIER, $GULP_SIZE, "
 NUM1="$(python -OO linear_fft_pipeline.py)"
 NUM2="$(python skcuda_fft_pipeline.py)"
-echo "Bifrost has $NUM1"
-echo "Scikit has $NUM2"
-echo "Bifrost is: "
-echo "scale=5; $NUM2/$NUM1" | bc
-echo "times faster"
+speedup=$(echo "scale=5; $NUM2/$NUM1" | bc)
+echo "$NUM1, $NUM2, $speedup, END<<<"
 
 done
 done
