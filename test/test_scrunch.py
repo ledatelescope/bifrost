e1082491f0fd27ef36a9a0d04009afe451129deb
Ben Barsdell <benbarsdell@gmail.com>
2017-07-23 00:09:23 -0700

    Clean up more copyright notices

--- test/test_scrunch.py
+++ test/test_scrunch.py
@@ -1,5 +1,4 @@
 # Copyright (c) 2016, The Bifrost Authors. All rights reserved.
-# Copyright (c) 2016, NVIDIA CORPORATION. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
--- tools/setirq.py
+++ tools/setirq.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 # Copyright (c) 2017, The Bifrost Authors. All rights reserved.
-# Copyright (c) 2017, NVIDIA CORPORATION. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
