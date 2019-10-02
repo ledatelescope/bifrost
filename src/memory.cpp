5d0a9e3d32d5325954e63caec1ec454b5327883e
jaycedowell <jayce.dowell@gmail.com>
2019-10-02 15:47:09 -0600

    Updated the dates in the files that were modified as part of this branch.

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 2016, The Bifrost Authors. All rights reserved.
+ * Copyright (c) 2016-2019, The Bifrost Authors. All rights reserved.
  * Copyright (c) 2016, NVIDIA CORPORATION. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
