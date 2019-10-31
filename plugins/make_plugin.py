032c7268740469ffb3f808a08e9b2ea976de96eb
jaycedowell <jayce.dowell@gmail.com>
2019-10-31 15:32:24 -0600

    Changed make_plugin.py so that it doesn't leave its Makefiles and objects files lying around.  Updated the makefile.tmpl to include the GPU archs to build.

--- plugins/make_plugin.py
+++ plugins/make_plugin.py
@@ -32,18 +32,7 @@ def main(args):
     makename = get_makefile_name(libname)
     
     # Part 1:  Build the Makefile
-    trigger_rebuild = args.force
-    if not os.path.exists(makename):
-        trigger_rebuild = True
-    elif os.path.getmtime(makename) < os.path.getmtime(__file__) \
-          or os.path.getmtime(makename) < os.path.getmtime('makefile.py') \
-          or os.path.getmtime(makename) < os.path.getmtime('makefile.tmpl') \
-          or os.path.getmtime(makename) < os.path.getmtime('wrap_plugin.py') \
-          or os.path.getmtime(makename) < os.path.getmtime('wrap_plugin.tmpl'):
-        trigger_rebuild = True
-        
-    if trigger_rebuild:
-        create_makefile(libname, incname, bifrost_path=args.bifrost_path)
+    create_makefile(libname, incname, bifrost_path=args.bifrost_path)
         
     # Part 2:  Build and wrap
     status = clean(makename)
@@ -52,15 +41,19 @@ def main(args):
     status = build(makename)
     if not status:
         sys.exit(status)
+        
+    # Part 3:  Clean up
+    os.unlink(makename)
+    objnames = glob.glob("%s*.o" % libname)
+    for objname in objnames:
+        os.unlink(objname)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Given a .cpp or .cu file that contains the source for a Bifrost plugin, create a Makefile, build the plugin, and create the Python wrappers needed to use the plugin.')
+        description='Given a .cpp or .cu file that contains the source for a Bifrost plugin, build the plugin and create the Python wrappers needed to use it.')
     parser.add_argument('filename', type=str,
                         help='filename to compile and wrap')
-    parser.add_argument('-f', '--force', action='store_true',
-                       help='force a rebuild of the Makefile')
     parser.add_argument('-b', '--bifrost-path', type=str, default=BIFROST_PATH,
                         help='path to the Bifrost source directory')
     args = parser.parse_args()
