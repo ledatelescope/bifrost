2e5e939912a48abd49dd03680124173b0c770927
Danny Price <dan@thetelegraphic.com>
2021-07-02 23:57:35 +0800

    Testing: ran 2to3 on xcorr_lite_test.py

--- test/xcorr_lite_test.py
+++ test/xcorr_lite_test.py
@@ -17,25 +17,25 @@ def test_xcorr(H, N, F, T):
     xcorr_bf = bf.ndarray(xcorr, dtype='f32', space='cuda')
 
     for ii in range(8):
-        print "---- Iteration %i ----" % ii
-        print "Generate test vectors..."
+        print("---- Iteration %i ----" % ii)
+        print("Generate test vectors...")
         d = np.random.randint(64, size=(H, F, N, T, 2), dtype='int8')
-        print "Data shape: ", d.shape
-        print "Xcorr shape:", xcorr.shape
+        print("Data shape: ", d.shape)
+        print("Xcorr shape:", xcorr.shape)
 
-        print "Computing Xcorr on CPU..."
+        print("Computing Xcorr on CPU...")
         xcorr_cpu = compute_xcorr_cpu(d)
 
-        print "Copying data to GPU..."
+        print("Copying data to GPU...")
         d_gpu     = bf.ndarray(d, dtype='i8', space='cuda')
 
-        print "Run xcorr_lite..."
+        print("Run xcorr_lite...")
         _bf.XcorrLite(d_gpu.as_BFarray(), xcorr_bf.as_BFarray(), np.int32(reset))
 
-        print "Copy result from GPU..."
+        print("Copy result from GPU...")
         xcorr_gpu = np.array(xcorr_bf.copy('system'))
         
-        print "Comparing CPU to GPU..."
+        print("Comparing CPU to GPU...")
         assert np.allclose(xcorr_gpu.squeeze(), xcorr_cpu.squeeze())
 
 def test_multi_accumulate(H, N, F, T, n_cycles):
@@ -46,28 +46,28 @@ def test_multi_accumulate(H, N, F, T, n_cycles):
 
     d = np.random.randint(64, size=(H, F, N, T, 2), dtype='int8')
 
-    print "Computing Xcorr on CPU..."
+    print("Computing Xcorr on CPU...")
     xcorr_cpu = compute_xcorr_cpu(d)
 
-    print "Running xcorr_lite..."
+    print("Running xcorr_lite...")
     d_gpu     = bf.ndarray(d, dtype='i8', space='cuda')
     _bf.XcorrLite(d_gpu.as_BFarray(), xcorr_bf.as_BFarray(), np.int32(reset))
     xcorr_gpu = np.array(xcorr_bf.copy('system'))
 
-    print "Testing first integration cycle..."    
+    print("Testing first integration cycle...")    
     assert np.allclose(xcorr_gpu.squeeze(), xcorr_cpu.squeeze())
     
-    print "Running loop ..."
+    print("Running loop ...")
     for ii in range(1, n_cycles):
-        print "Run xcorr_lite..."
+        print("Run xcorr_lite...")
         reset = 0
         _bf.XcorrLite(d_gpu.as_BFarray(), xcorr_bf.as_BFarray(), np.int32(reset))
 
-        print "Copy result from GPU..."
+        print("Copy result from GPU...")
         xcorr_gpu = np.array(xcorr_bf.copy('system'))
         xcorr_cpu += compute_xcorr_cpu(d)
 
-        print "Testing integration cycle %i /%i..." % (ii+1, n_cycles)    
+        print("Testing integration cycle %i /%i..." % (ii+1, n_cycles))    
         assert np.allclose(xcorr_gpu.squeeze(), xcorr_cpu.squeeze())
 
 if __name__ == "__main__":
