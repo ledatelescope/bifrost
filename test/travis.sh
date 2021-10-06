5fe1749126733a92a7058cc50c67d8f1c6b85954
jaycedowell <jayce.dowell@gmail.com>
2021-10-06 13:25:52 -0600

    Move the telemetry.disable() around.

--- test/travis.sh
+++ test/travis.sh
@@ -2,6 +2,7 @@
 # This file runs CPU-safe tests for travis-ci
 ./download_test_data.sh
 export LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
+python -c "from bifrost import telemetry; telemetry.disable()"
 coverage run --source=bifrost.ring,bifrost,bifrost.pipeline -m unittest \
   test_block \
   test_sigproc \
