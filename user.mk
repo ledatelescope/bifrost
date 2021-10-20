f13f6d900024b80a1deaa98d46144b3046a71d7a
Danny Price <dan@thetelegraphic.com>
2021-10-20 11:32:37 +0800

    Updates to user.mk

--- user.mk
+++ user.mk
@@ -1,16 +1,19 @@
-
 CXX           ?= g++
 NVCC          ?= nvcc
 LINKER        ?= g++
-CXX_FLAGS     ?= -O3 -Wall 
-NVCC_FLAGS    ?= -O3 -Xcompiler "-Wall" --ptxas-options=-v
-LINKER_FLAGS  ?=
+CPPFLAGS      ?=
+CXXFLAGS      ?= -O3 -Wall -pedantic
+NVCCFLAGS     ?= -O3 -Xcompiler "-Wall" #-Xptxas -v
+LDFLAGS       ?=
+DOXYGEN       ?= doxygen
+PYBUILDFLAGS   ?=
+PYINSTALLFLAGS ?=
 
+  
+  
+GPU_ARCHS     ?= 60 61
 
 GPU_SHAREDMEM ?= 16384 # GPU shared memory size
-
-#GPU_ARCHS     ?= 52
-GPU_ARCHS     ?= 60 61
 
 CUDA_HOME     ?= /usr/local/cuda
 CUDA_LIBDIR   ?= $(CUDA_HOME)/lib
@@ -19,15 +22,11 @@ CUDA_INCDIR   ?= $(CUDA_HOME)/include
 
 ALIGNMENT ?= 4096 # Memory allocation alignment
 
-PSRDADA_PATH  ?= /home/npsr/software/psrdada/latest
-BUILDDP4A   = 1  # Build beanfarmer + xcorr_lite
-BUILDROMEIN   = 1  # Build Romein kernels
 #NODEBUG    = 1 # Disable debugging mode (use this for production releases)
-#NOUDPSOCKET = 1  # Disable UDP code compilation
-TRACE      = 1 # Enable tracing mode (generates annotations for use with nvprof/nvvp)
+#TRACE      = 1 # Enable tracing mode (generates annotations for use with nvprof/nvvp)
 #NOCUDA     = 1 # Disable CUDA support
-ANY_ARCH   = 1 # Disable native architecture compilation
+#ANY_ARCH   = 1 # Disable native architecture compilation
 #CUDA_DEBUG = 1 # Enable CUDA debugging (nvcc -G)
-NUMA       = 1 # Enable use of numa library for setting affinity of ring memory
-HWLOC      = 1 # Enable use of hwloc library for memory binding in udp_capture
+#NUMA       = 1 # Enable use of numa library for setting affinity of ring memory
+#HWLOC      = 1 # Enable use of hwloc library for memory binding in udp_capture
 #VMA        = 1 # Enable use of Mellanox libvma in udp_capture
