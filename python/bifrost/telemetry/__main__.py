3ec66a797bcb63cae8fc46497f442762068c8871
jaycedowell <jaycedowell@gmail.com>
2022-03-22 16:00:46 -0600

    Boilerplate.

--- python/bifrost/telemetry/__main__.py
+++ python/bifrost/telemetry/__main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2021, The Bifrost Authors. All rights reserved.
-# Copyright (c) 2021, The University of New Mexico. All rights reserved.
+# Copyright (c) 2021-2022, The Bifrost Authors. All rights reserved.
+# Copyright (c) 2021-2022, The University of New Mexico. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
