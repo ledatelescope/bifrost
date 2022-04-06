c7db268b9cdcecbd24701ff713c3c51aa450ee72
jaycedowell <jayce.dowell@gmail.com>
2022-04-05 18:43:08 -0600

    Use numpy.ndarray.copy() when we can.

--- python/bifrost/ndarray.py
+++ python/bifrost/ndarray.py
@@ -389,7 +389,28 @@ class ndarray(np.ndarray):
         if space is None:
             space = self.bf.space
         if not self.flags['C_CONTIGUOUS']:
-            raise NotImplementedError('Only C contiguous arrays are supported')
+            # Deal with arrays that need to have their layouts changed
+            if space_accessible(space, ['system']):
+                ## For arrays that can be accessed from the system space, use
+                ## numpy.ndarray.copy() to do the heavy lifting
+                if space == 'cuda_managed':
+                    # TODO: Decide where/when these need to be called
+                    device.stream_synchronize()
+                temp = np.array(self)
+                return ndarray(temp.copy(order=order), space=space)
+            else:
+                # TODO: Is this as robust as numpy.ndarray.copy()?
+                """
+                ## For arrays that can be access from CUDA, use bifrost.tranpose
+                ## to do the heavy lifting
+                permute = np.argsort(self.strides)[::-1]
+                temp = ndarray(self.shape, dtype=self.dtype, space=space)
+                array_type = ctypes.c_int * self.ndim
+                axes_array = array_type(*permute)
+                _check(_bf.bfTranspose(self.as_BFarray(), temp.as_BFarray(), axes_array))
+                return temp
+                """
+                raise NotImplementedError('For %s only C contiguous arrays are supported' % space)
         # Note: This makes an actual copy as long as space is not None
         return ndarray(self, space=space)
     def _key_returns_scalar(self, key):
