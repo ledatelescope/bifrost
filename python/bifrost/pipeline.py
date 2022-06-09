de9ff5401100a5f0e281d9793df67a94d7698747
jaycedowell <jayce.dowell@gmail.com>
2022-06-08 18:40:59 -0600

    Revert, sorta.

--- python/bifrost/pipeline.py
+++ python/bifrost/pipeline.py
@@ -373,11 +373,11 @@ class Block(BlockScope):
             active_orings = self.begin_writing(oring_stack, self.orings)
             try:
                 self.main(active_orings)
-            except Exception:
+            except Exception as e:
                 self.pipeline.block_init_queue.put((self, False))
                 sys.stderr.write("From block instantiated here:\n")
                 sys.stderr.write(self.init_trace)
-                return
+                raise e
     def num_outputs(self):
         # TODO: This is a little hacky
         return len(self.orings)
