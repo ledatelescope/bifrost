bac239700ec811e70489b1d0bebb518eca3cbfa6
Christopher League <league@contrapunctus.net>
2022-07-28 14:37:36 -0400

    flake.lock: Update
    • Updated input 'nixpkgs':
        'path:/nix/store/3ck36g7rh9v9fa924h149pq7h6cx9nzx-source' (2022-01-12)
      → 'github:NixOS/nixpkgs/8f73de28e63988da02426ebb17209e3ae07f103b' (2022-07-27)
    • Updated input 'pre-commit-hooks':
        'github:cachix/pre-commit-hooks.nix/ff9c0b459ddc4b79c06e19d44251daa8e9cd1746' (2021-12-18)
      → 'github:cachix/pre-commit-hooks.nix/f436e6dbc10bb3500775785072a40eefe057b18e' (2022-07-23)
    • Updated input 'pre-commit-hooks/flake-utils':
        'github:numtide/flake-utils/2ebf2558e5bf978c7fb8ea927dfaed8fefab2e28' (2021-04-25)
      → 'github:numtide/flake-utils/3cecb5b042f7f209c56ffd8371b2711a290ec797' (2022-02-07)
    
    Revised for nixpkgs rename 'cudatoolkit_10' → 'cudaPackages_10.cudatoolkit'.

--- flake.nix
+++ flake.nix
@@ -8,8 +8,7 @@
   };
 
   inputs.pre-commit-hooks = {
-    url =
-      "github:cachix/pre-commit-hooks.nix/ff9c0b459ddc4b79c06e19d44251daa8e9cd1746";
+    url = "github:cachix/pre-commit-hooks.nix";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
@@ -231,7 +230,7 @@
           bifrost = final.callPackage bifrost { };
           bifrost-doc = final.callPackage bifrost-doc { };
           github_stats = final.writeShellScriptBin "github_stats" ''
-            ${final.python3.withPackages (p: [p.PyGithub])}/bin/python \
+            ${final.python3.withPackages (p: [ p.PyGithub ])}/bin/python \
               ${tools/github_stats.py} "$@"
           '';
         }
@@ -247,10 +246,10 @@
           # the default 10 and 11. It's easy to generate other point releases
           # from the overlay. (Versions prior to 10 are not supported anymore by
           # nixpkgs.)
-          isCuda = name: builtins.match "cudatoolkit(_1[01])" name != null;
-          shortenCuda = lib.replaceStrings [ "toolkit" "_" ] [ "" "" ];
-          cudaAttrs = lib.filterAttrs
-            (name: pkg: isCuda name && lib.elem pkgs.system pkg.meta.platforms)
+          isCuda = name: builtins.match "cudaPackages(_1[01])" name != null;
+          shortenCuda = lib.replaceStrings [ "Packages" "_" ] [ "" "" ];
+          cudaAttrs = lib.filterAttrs (name: pkg:
+            isCuda name && lib.elem pkgs.system pkg.cudatoolkit.meta.platforms)
             pkgs;
 
           # Which C++ compilers can we build with? How to name them?
@@ -282,7 +281,7 @@
                     + lib.optionalString enableDebug "-debug") {
                       inherit stdenv enableDebug;
                       enableCuda = cuda != null;
-                      cudatoolkit = pkgs.${cuda};
+                      cudatoolkit = pkgs.${cuda}.cudatoolkit;
                     })));
 
           # Runnable ctypesgen per python. Though it's just the executable we
